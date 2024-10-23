6fb21f912015e2708d1268dc9df95760f3001770
jorginho222 <rodizioperalta22@gmail.com>
2024-10-23 17:11:16 -0300

    Start project. Add transport model and table

---
+++ database/migrations/2024_10_23_184252_create_transports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transports', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('code');
+            $table->float('temperature');
+            $table->float('capacity');
+            $table->boolean('active');
+            $table->string('availability');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transports');
+    }
+};
